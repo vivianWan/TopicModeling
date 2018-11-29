37f1874120cea1ea531617b8731fe2cc4dc889d9
Vivianwan <wei.wan@uni3t.com>
2018-11-29 15:26:29 -0500

    add execution time

--- topicModeling.py
+++ topicModeling.py
@@ -8,12 +8,14 @@ from sklearn.feature_extraction.text import TfidfVectorizer, CountVectorizer
 from sklearn.datasets import fetch_20newsgroups
 from sklearn.decomposition import NMF, LatentDirichletAllocation
 
+from time import time
+
 def display_topics(model, feature_names, no_top_words):
     for topic_idx, topic in enumerate(model.components_):
         print ("Topic %d:" % (topic_idx))
         print (" ".join([feature_names[i] for i in topic.argsort()[:-no_top_words -1:-1]]))
         
-
+t0 = time()
 dataset = fetch_20newsgroups(shuffle=True, random_state=1, remove = (
         'headers','footers','quotes'))
 documents = dataset.data
@@ -43,4 +45,5 @@ lda = LatentDirichletAllocation(n_components=no_topics, max_iter=5, learning_met
 
 no_top_words = 10
 display_topics(nmf, tfidf_feature_names, no_top_words)
-display_topics(lad, tf_feature_names, no_top_words)
+display_topics(lda, tf_feature_names, no_top_words)
+print ("The execute time is: ", time()-t0)
