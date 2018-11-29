9fa8e9fa147f75d43181c70ea619de8fb1ccebae
Vivianwan <wei.wan@uni3t.com>
2018-11-29 15:09:27 -0500

    fix bugs

--- topicModeling.py
+++ topicModeling.py
@@ -21,7 +21,7 @@ documents = dataset.data
 no_features = 1000
 
 # NMF is able to use tf-idf
-tfidf_vectorizer = TfidfVectorizer(max_df=0.95, min_df=2, max_features=n_features,
+tfidf_vectorizer = TfidfVectorizer(max_df=0.95, min_df=2, max_features=no_features,
                                     stop_words='english')
 tfidf=tfidf_vectorizer.fit_transform(documents)
 tfidf_feature_names = tfidf_vectorizer.get_feature_names()
